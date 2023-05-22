ee6843de43513e72f1c19e8252a65b563b9c3e55
hansmann <wolfgang.hansmann@telekom.de>
2023-05-22 13:00:58 +0200

    Added HTML output

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import random
 
 app = FastAPI()
@@ -9,5 +9,18 @@ with open("excuses.txt") as fh:
     excuses = fh.read().split("\n")
 
 @app.get("/")
-async def root():
-    return {"excuse": excuses[random.randint(0, len(excuses) - 1)]}
+async def root(output="json"):
+    excuse = excuses[random.randint(0, len(excuses) - 1)]
+    if output == "json":
+        return {"excuse": excuse}
+    else:
+    	return Response(content=f"""
+<html>
+  <body>
+  <h1>Excuse of the day:</h1>
+    <p>
+      <b>{excuse}</b>
+    </p>
+  </body>
+</html>
+""", media_type="text/html")

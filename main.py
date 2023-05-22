e0803f2839ef6d7cf5067eb0baa3dff0abb50bf8
Wolfgang Hansmann <wolfgang.hansmann@telekom.de>
2023-05-22 09:47:07 +0100

    Added excuses

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # main.py
 
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
+with open("excuses.txt") as fh:
+    excuses = fh.read().split("\n")
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"excuse": excuses[random.randint(0, len(excuses))]}
